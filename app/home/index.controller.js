d340674c4d445e340dbb5fb248df90c64156d740
Vebito Tetseo <tetseovebito@gmail.com>
2016-02-02 15:48:13 +0530

    Added an employee list table in admin's home page

--- app/home/index.controller.js
+++ app/home/index.controller.js
@@ -1,13 +1,13 @@
-﻿(function () {
+(function () {
     'use strict';
 
     angular
         .module('app')
         .controller('Home.IndexController', Controller);
 
-    function Controller(UserService) {
+    function Controller(UserService, $scope, uiGridConstants) {
         var vm = this;
-        
+
         vm.users = null;
         vm.trainings = null;
 
@@ -25,16 +25,48 @@
             //     vm.training = training;
             // });
         }
+        $scope.gridOptions = {
+            enablePaginationControls: true,
+            paginationPageSizes: [2, 5, 10, 20, 50],
+            paginationPageSize: 10,
+            enableGridMenu: true,
+            enableFiltering: true,
+            columnDefs: [
+                {
+                    field: "_id",
+                    displayName: "EmployeeId",
+                    sort: {
+                        direction: uiGridConstants.DESC,
+                        priority: 0
+                    }
+                 },
+                {
+                    field: "firstName",
+                    displayName: "First Name"
+                },
+                {
+                    field: "lastName",
+                    displayName: "Last Name"
+                },
+                {
+                    field: "creditPoints",
+                    displayName: "Points"
+                }
+                 ],
+            onRegisterApi: function (gridApi) {
+                $scope.gridApi = gridApi;
+            }
+        };
     }
     // function Controller(TrainingService) {
     //     var vm = this;
-      
+
     //     vm.training = null;
 
     //     initController();
 
     //     function initController() {
-    
+
 
     //         // get trainings
     //         TrainingService.GetAll().then(function (training) {
